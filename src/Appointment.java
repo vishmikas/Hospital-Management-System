f3e015ca129ee0e3ccdf7c6e03a88b4eb6fbf704
Dilum Samarathunga <75325717+vishmikas@users.noreply.github.com>
2025-12-28 23:26:53 +0530

    Created Appointment Class

---
+++ src/Appointment.java
@@ -0,0 +1,43 @@
+import java.time.LocalDate;
+
+public class Appointment {
+
+    private Patient patient;
+    private Doctor doctor;
+    private LocalDate date;
+
+    public Appointment(Patient patient, Doctor doctor, LocalDate date) {
+        this.patient = patient;
+        this.doctor = doctor;
+        this.date = date;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Patient: " + patient + ", Doctor: " + doctor + ", Date: " + date;
+    }
+}
