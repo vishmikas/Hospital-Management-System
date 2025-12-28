b80f85de230c72bfbd98edbb98cc228a854d98d9
Dilum Samarathunga <75325717+vishmikas@users.noreply.github.com>
2025-12-28 23:15:07 +0530

    Created Patient Class

---
+++ src/Patient.java
@@ -0,0 +1,42 @@
+public class Patient extends User{
+
+    private int age;
+    private String address;
+    private String phoneNumber;
+
+    public Patient(String name, int age, String address, String phoneNumber) {
+        super(name, age);
+        this.age = age;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Age: " + age + " Address: " + address + " Phone: " + phoneNumber;
+    }
+}
